02da59a3a34d6c79dd1db76f09ea5626c8ee14d0
SybrenM <44946268+SybrenM@users.noreply.github.com>
2019-11-27 12:39:06 +0100

    Merged

---
+++ Code/Wijkagent/Wijkagent/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wijkagent
+{
+    class Person
+    {
+        public string naam { get; set; }
+        public int leeftijd { get; set; }
+        public string bsn { get; set; }
+    }
+}
