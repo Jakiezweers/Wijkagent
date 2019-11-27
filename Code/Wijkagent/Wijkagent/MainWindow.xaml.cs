bd6b76c7530aa7e6c65b4d842f4d0b597c75ec90
jake zweers <jakezw96@hotmail.com>
2019-11-27 11:08:16 +0100

    Removed

--- Code/Wijkagent/Wijkagent/MainWindow.xaml.cs
+++ Code/Wijkagent/Wijkagent/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Wijkagent.Classes;
 
 namespace Wijkagent
 {
@@ -20,9 +23,38 @@ namespace Wijkagent
     /// </summary>
     public partial class MainWindow : Window
     {
+
         public MainWindow()
         {
             InitializeComponent();
+            
+        }
+
+
+        public async Task sendFileAsync(String file_data, string filename)
+        {
+            Uploader upload = new Uploader();
+
+
+            Random r = new Random();
+            String resp = await upload.SendFileAsync(file_data, filename, "icon/", r.Next(0,10000000)+ ".png");
+            LblResp.Content = resp;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image files (*.png;*.jpeg)|*.png;*.jpeg|All files (*.*)|*.*";
+            if (openFileDialog.ShowDialog() == true)
+            {
+
+
+                byte[] imageArray = System.IO.File.ReadAllBytes(openFileDialog.FileName);
+                string base64ImageRepresentation = Convert.ToBase64String(imageArray);
+                this.sendFileAsync(base64ImageRepresentation, openFileDialog.FileName);
+
+
+            }
         }
     }
 }
