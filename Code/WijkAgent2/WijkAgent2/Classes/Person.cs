86a1e1036f0b488a7af5c1eb7f073c9f3dfd68f1
Thomas van Strijland <thomasvanstrijland@gmail.com>
2019-12-02 12:19:53 +0100

    Added feixiang and sybren code.

--- Code/WijkAgent2/WijkAgent2/Classes/Person.cs
+++ Code/WijkAgent2/WijkAgent2/Classes/Person.cs
@@ -8,8 +8,10 @@ namespace Wijkagent2.Classes
 {
     public class Person
     {
-        public string naam { get; set; }
-        public int leeftijd { get; set; }
+        public int firstnamecount { get; set; }
+        public string firstname { get; set; }
+        public string lastname { get; set; }
+        public DateTime birthdate { get; set; }
         public int bsn { get; set; }
         public string type { get; set; }
         public int person_id { get; set; }
--- Code/WijkAgent2/WijkAgent2/Pages/delicten/delicten_list.xaml.cs
+++ Code/WijkAgent2/WijkAgent2/Pages/delicten/delicten_list.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Data;
 using System.Data.Common;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,6 +26,8 @@ namespace WijkAgent2.Pages.delicten
     public partial class delicten_list : Page
     {
         MainWindow mw;
+        int i;
+        int j;
         public delicten_list(MainWindow MW)
         {
             mw = MW;
@@ -35,52 +39,86 @@ namespace WijkAgent2.Pages.delicten
 
             using (DbConnection connection = factory.CreateConnection())
             {
-                if (connection == null)
-                {
-                    Console.WriteLine("connection Error");
-                    Console.ReadLine();
-                    return;
-                }
-                Console.WriteLine("connection geslaagd");
-
                 connection.ConnectionString = connectionstring;
                 connection.Open();
                 DbCommand command = factory.CreateCommand();
-                if (command == null)
-                {
-                    Console.WriteLine("geen command gegeven");
-                    Console.ReadLine();
-                    return;
-                }
 
                 command.Connection = connection;
-                command.CommandText = "SELECT * FROM dbo.delict " +
-                                      "WHERE status = 1";
+                command.CommandText = "SELECT DISTINCT delict.delict_id, delict.street, delict.added_date, COUNT(person.firstname) as firstname, COUNT(person.lastname) FROM dbo.delict LEFT JOIN dbo.delict_person ON delict.delict_id = delict_person.delict_id LEFT JOIN dbo.person ON person.person_id = delict_person.delict_person_id WHERE delict.status = 1 GROUP BY delict.delict_id, delict.street, delict.added_date";
 
                 using (DbDataReader dataReader = command.ExecuteReader())
                 {
                     while (dataReader.Read())
                     {
                         int id = Convert.ToInt32(dataReader["delict_id"]);
+                        int count = Convert.ToInt32(dataReader["firstname"]);
                         Delict d1 = new Delict();
                         d1.id = id;
                         d1.street = (string)dataReader["street"];
                         d1.createtime = (DateTime)dataReader["added_date"];
+
+                        d1.firstnamecount = count;
                         Console.WriteLine($"{dataReader["street"]}");
                         Delicten.Items.Add(d1);
                     }
                 }
             }
         }
-
-        private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-
-        }
         private void Activate(object sender, RoutedEventArgs e)
         {
-            var myValue = ((System.Windows.Controls.Button)sender).Tag;
-            Console.WriteLine("ID: " + myValue);
+            MessageBoxResult dialogResult = MessageBox.Show("Wil u dit delict archiveren?", "Archiveren", MessageBoxButton.YesNo);
+            if (dialogResult == MessageBoxResult.Yes)
+            {
+                string provider = ConfigurationManager.AppSettings["provider"];
+                string connectionstring = ConfigurationManager.AppSettings["connectionString"];
+
+                DbProviderFactory factory = DbProviderFactories.GetFactory(provider);
+
+                using (DbConnection connection = factory.CreateConnection())
+                {
+                    connection.ConnectionString = connectionstring;
+                    connection.Open();
+                    DbCommand command = factory.CreateCommand();
+
+                    command.Connection = connection;
+
+                    var myValue = ((System.Windows.Controls.Button)sender).Tag;
+                    string statusChange = "UPDATE delict " +
+                                     "SET status = 0 " +
+                                     "WHERE delict_id = @delictID";
+                    //TODO add use id 
+                    string addToArchive = "INSERT INTO dbo.archive (delict_id, date_added) " +
+                                          "VALUES (@delictID, @Datetime)"; 
+                    using (SqlConnection cnn = new SqlConnection(connectionstring))
+                    {
+                        try
+                        {
+                            cnn.Open();
+                            using (SqlCommand cmd = new SqlCommand(statusChange, cnn))
+                            {
+                                cmd.Parameters.Add("@delictID", SqlDbType.NVarChar).Value = myValue;
+                                cmd.ExecuteNonQuery();
+                            }
+                            using (SqlCommand cmd = new SqlCommand(addToArchive, cnn))
+                            {
+                                cmd.Parameters.Add("@delictID", SqlDbType.NVarChar).Value = myValue;
+                                cmd.Parameters.Add("@Datetime", SqlDbType.DateTime).Value = DateTime.Today;
+                                cmd.ExecuteNonQuery();
+                            }
+
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show("ERROR:" + ex.Message);
+                        }
+                        Console.WriteLine("ID: " + myValue);
+                    }
+                }
+            }
+            else if (dialogResult == MessageBoxResult.No)
+            {
+                //do something else
+            }
         }
 
         private void ViewDelict(object sender, RoutedEventArgs e)
@@ -92,6 +130,68 @@ namespace WijkAgent2.Pages.delicten
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             mw.LoadHomeScreen();
+        }
+        private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string provider = ConfigurationManager.AppSettings["provider"];
+            string connectionstring = ConfigurationManager.AppSettings["connectionString"];
+            var iddelict = Delicten.SelectedItem as Delict;
+            DbProviderFactory factory = DbProviderFactories.GetFactory(provider);
+            using (DbConnection connection = factory.CreateConnection())
+            {
+                connection.ConnectionString = connectionstring;
+                connection.Open();
+                DbCommand command = factory.CreateCommand();
+                command.Connection = connection;
+                command.CommandText = "SELECT firstname FROM dbo.person JOIN dbo.delict_person ON person.person_id = delict_person.person_id WHERE delict_person.delict_id =" + iddelict.id;
+                personnames.Items.Clear();
+                using (DbDataReader dataReader = command.ExecuteReader())
+                {
+                    while (dataReader.Read())
+                    {
+                        personnames.Items.Add(dataReader["firstname"]);
+                    }
+                }
+            }
+
+        }
+        private void KeyDownEvent(object sender, KeyEventArgs e)
+        {
+            i = Delicten.SelectedIndex;
+            if (e.Key == Key.Right)
+            {
+                if (Delicten.SelectedIndex + 10 < Delicten.Items.Count)
+                {
+                    i = i + 10;
+                    Delicten.SelectedItem = Delicten.Items[i];
+                    Delicten.ScrollIntoView(Delicten.SelectedItem);
+                    Delicten.Focus();
+                    Console.WriteLine(Delicten.SelectedIndex.ToString());
+                }
+                else
+                {
+                    Delicten.SelectedItem = Delicten.Items[Delicten.Items.Count - 1];
+                    Delicten.ScrollIntoView(Delicten.SelectedItem);
+                    Delicten.Focus();
+                }
+            }
+
+            if (e.Key == Key.Left)
+            {
+                if (Delicten.SelectedIndex - 10 > 0)
+                {
+                    i = i - 10;
+                    Delicten.SelectedItem = Delicten.Items[i];
+                    Delicten.ScrollIntoView(Delicten.SelectedItem);
+                    Delicten.Focus();
+                }
+                else
+                {
+                    Delicten.SelectedItem = Delicten.Items[0];
+                    Delicten.ScrollIntoView(Delicten.SelectedItem);
+                    Delicten.Focus();
+                }
+            }
         }
     }
 }
